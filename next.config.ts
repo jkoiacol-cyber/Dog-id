acce6583060facfbdf55c67ebab25e4259c297e2
jkoiacol-cyber <jkoiacol@gmail.com>
2026-05-28 19:39:43 +0200

    fix: Se fixea el problema de redireccion

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,31 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
+      // 1. Si viene slug, NO redirigir a /landing
       {
         source: '/',
         has: [
           {
             type: 'query',
             key: 'slug',
-            value: '(.*)',
           },
         ],
-        destination: '/', // si hay slug, NO redirigimos
+        destination: '/',
         permanent: false,
       },
+      {
+        source: '/',
+        has: [
+          {
+            type: 'query',
+            key: 'secret_id',
+          },
+        ],
+        destination: '/',
+        permanent: false,
+      },
+
+      // 2. Si NO viene slug ni secret_id → redirigir a /landing
       {
         source: '/',
         destination: '/landing',
