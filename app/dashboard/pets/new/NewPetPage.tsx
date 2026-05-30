92d694badd319dc5b565cb95be7a899f72dd18b2
jkoiacol-cyber <jkoiacol@gmail.com>
2026-05-30 09:55:22 +0200

    fix: Se añade cookies de sesion para no enviar emails1

--- app/dashboard/pets/new/NewPetPage.tsx
+++ app/dashboard/pets/new/NewPetPage.tsx
@@ -443,7 +443,7 @@ export default function NewPetPage() {
     setSaving(false);
     // Se añade const res en la sigueinte linea 
     try {
-      const res await fetch("/api/send-welcome-email", {
+      const res = await fetch("/api/send-welcome-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
